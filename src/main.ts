feb5597380826c39a12c2e8dd44e79542f156a80
cvirgili438 <100769556+cvirgili438@users.noreply.github.com>
2023-02-13 23:15:47 -0300

    'some'

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config/dist';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as morgan from 'morgan'
+import { CORS } from './constants';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'))
+
+  const configService = app.get(ConfigService)
+  app.enableCors(CORS)
+  app.setGlobalPrefix('api')
+  
+  await app.listen(configService.get('PORT'), ()=>{
+    console.log(`Server is running on port ${configService.get('PORT')}`)
+    
+  });
+}
+bootstrap();
---
+++ src/users/users.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { UsersService } from './service/users.service';
+import { UsersController } from './controllers/users.controller';
+
+@Module({
+  providers: [ UsersService],
+  controllers: [UsersController]
+})
+export class UsersModule {}
