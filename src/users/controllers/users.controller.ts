19a7dc790238e84186287d46edb87b2c0932be64
Carlos <cvirgili438@gmail.com>
2023-03-16 21:43:10 -0300

    adding header

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -7,39 +7,53 @@ import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UserIdGuard } from 'src/auth/guards/user-id.guard';
 import { UserCreateDTO, UserDTO, UserToScheduleDTO, UserUpdateDTO } from '../dto/user.dto';
 import { UsersService } from '../service/users.service';
-import {ApiTags} from '@nestjs/swagger'
+import {ApiTags,ApiHeader} from '@nestjs/swagger'
 @ApiTags('Users')
 @Controller('users')
-@UseGuards(AuthGuard,RolesGuard,UserIdGuard)
+@UseGuards(AuthGuard, RolesGuard, UserIdGuard)
 export class UsersController {
-    constructor(private readonly userService : UsersService){}
-    @PublicAccess()
-    @Post('register')   
-    public async registerUser(@Body() body : UserCreateDTO){
-        return await this.userService.createUser(body)
-    }
-    @Post('add-Schedule')
-    public async addSchedule(@Body() body :UserToScheduleDTO){
-        return await this.userService.relationToSchedule(body)
-    }
-    
-    @Roles('SUPERVISOR','ADMIN')
-    @Get('all')
-    public async findAllUsers(){
-        return await this.userService.findUsers()
-    }
-    @PublicAccess()
-    @Get(':id')
-    public async findUserById(@Param('id') id : string){
-        return await this.userService.findUserById(id)
-    }
-    @idUserDecorator('USERID')
-    @Put('edit/:id')
-    public async updateUser(@Param('id') id:string,@Body() body:UserUpdateDTO){
-        return await this.userService.updateUser(body,id)
-    }
-    @Delete('delete/:id')
-    public async deleteUser(@Param('id') id:string){
-        return await this.userService.deleteUser(id)
-    }
+  constructor(private readonly userService: UsersService) {}
+  @PublicAccess()
+  @Post('register')
+  public async registerUser(@Body() body: UserCreateDTO) {
+    return await this.userService.createUser(body);
+  }
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Post('add-Schedule')
+  public async addSchedule(@Body() body: UserToScheduleDTO) {
+    return await this.userService.relationToSchedule(body);
+  }
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Roles('SUPERVISOR', 'ADMIN')
+  @Get('all')
+  public async findAllUsers() {
+    return await this.userService.findUsers();
+  }
+  @PublicAccess()
+  @Get(':id')
+  public async findUserById(@Param('id') id: string) {
+    return await this.userService.findUserById(id);
+  }
+  @idUserDecorator('USERID')
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Put('edit/:id')
+  public async updateUser(
+    @Param('id') id: string,
+    @Body() body: UserUpdateDTO,
+  ) {
+    return await this.userService.updateUser(body, id);
+  }
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Delete('delete/:id')
+  public async deleteUser(@Param('id') id: string) {
+    return await this.userService.deleteUser(id);
+  }
 }
--- src/workplaces/controllers/work-places.controller.ts
+++ src/workplaces/controllers/work-places.controller.ts
@@ -5,32 +5,46 @@ import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { ByNameDTO, updateWorkPlacesDTO, workPlacesDTO } from '../dto/workPlaces.dto';
 import { WorkPlacesService } from '../service/work-places.service';
-import {ApiTags} from '@nestjs/swagger'
+import {ApiTags, ApiHeader} from '@nestjs/swagger'
 
 @ApiTags('Work Places')
 @Controller('work-places')
-@UseGuards(AuthGuard,RolesGuard)
+@UseGuards(AuthGuard, RolesGuard)
 export class WorkPlacesController {
-constructor(private readonly workPlacesServices: WorkPlacesService){}
-    @Roles('ADMIN')
-    @Post('create')
-        public async createWorkPlace(@Body() body:workPlacesDTO){
-            return await this.workPlacesServices.createWorkPlace(body)
-        }
-    @Roles('SUPERVISOR','ADMIN')
-    @Get('all')
-        public async getAllWorkPlaces(){
-            return await this.workPlacesServices.findWorkPlaces()
-        }
-    @Roles('SUPERVISOR','ADMIN')
-    @Get('/name/')
-        public async getPlaceByCity(@Body() body:ByNameDTO){
-            return await this.workPlacesServices.getByName(body)
-        }
-    @Roles('ADMIN')
-    @Put(':id')
-        public async changePlace(@Body()body :updateWorkPlacesDTO, @Param('id') id:string){
-            return await this.workPlacesServices.updateWorkPlace(body,id)
-    }
-
+  constructor(private readonly workPlacesServices: WorkPlacesService) {}
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Roles('ADMIN')
+  @Post('create')
+  public async createWorkPlace(@Body() body: workPlacesDTO) {
+    return await this.workPlacesServices.createWorkPlace(body);
+  }
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Roles('SUPERVISOR', 'ADMIN')
+  @Get('all')
+  public async getAllWorkPlaces() {
+    return await this.workPlacesServices.findWorkPlaces();
+  }
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Roles('SUPERVISOR', 'ADMIN')
+  @Get('/name/')
+  public async getPlaceByCity(@Body() body: ByNameDTO) {
+    return await this.workPlacesServices.getByName(body);
+  }
+  @ApiHeader({
+    name: 'access_token',
+  })
+  @Roles('ADMIN')
+  @Put(':id')
+  public async changePlace(
+    @Body() body: updateWorkPlacesDTO,
+    @Param('id') id: string,
+  ) {
+    return await this.workPlacesServices.updateWorkPlace(body, id);
+  }
 }
